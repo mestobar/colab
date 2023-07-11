3555994daa5e4e2d4edea37128f694db231fe31c
Maximiliano Estobar <mestobar@gmail.com>
2023-07-11 18:40:25 +0000

    course.html añadido

--- colabapp/academy/urls.py
+++ colabapp/academy/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import v_course
 
 urlpatterns = [
+
+    path('course', v_course)
 
 ]
--- colabapp/academy/views.py
+++ colabapp/academy/views.py
@@ -8,3 +8,11 @@ def v_index(request):
     }
 
     return render(request, 'index.html', context)
+
+def v_course(request):
+    context = {
+
+    }
+    return render(request, 'course.html', context)
+
+    
