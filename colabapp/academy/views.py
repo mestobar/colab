293ab843c260998adc441e85b1215d18d3d9435c
Maximiliano Estobar <mestobar@gmail.com>
2023-07-14 23:20:53 +0000

    message creado

--- colabapp/academy/views.py
+++ colabapp/academy/views.py
@@ -27,15 +27,18 @@ def v_subscribe(request, course_id):
 
     subject = Subject.objects.filter(course_id=course_id).last()
     if subject is None:
+         messages.error(request, "No puedes suscribirte a este curso.") 
          return HttpResponseRedirect("/academy/course/%s" % (course_id))
 
     verificar = Subscription.objects.filter(subject_id = subject.id, student_id = request.user.id)
     if verificar.exists():
+        messages.success(request, "Tu subscripción ya está activa.")
         return HttpResponseRedirect("/academy/course/%s" % (course_id))
     else:
         subs = Subscription()
         subs.student_id = request.user.id
         subs.subject_id = subject.id
         subs.save()
+        messages.success(request, "Felicidades, acabas de suscribirte!!")
         return HttpResponseRedirect("/academy/course/%s" % (course_id))
 
